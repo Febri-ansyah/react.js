f8b39fdc979cea8820b1e08bd71cb0f8cb307af6
Febri-ansyah <febriansyahansyah27@gmail.com>
2023-09-02 12:17:15 +0700

    materi satu

---
+++ freact/src/components/TesComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TesComponent extends React.Component{
+  render(){
+  	return(
+			<div>
+				<h1> Ini Cara Belajar React Js Untuk Pemula</h1>
+				<p>Paduan Untuk Membuat Komponen dengan class</p>
+			</div>
+		);
+	}
+}
+export default TesComponent;
